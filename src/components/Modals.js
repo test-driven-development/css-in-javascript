62e5f8ed20e3226dc1e72f007aef10706b677712
wpannell <wil.pannell@gmail.com>
2022-08-29 12:44:57 -0700

    stub modal

---
+++ src/components/Modals.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ModalWrapper = styled.div`
+  width: 800px;
+  height: 500px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background-color: ${props => props.theme.formElementBackground};
+  color: ${props => props.theme.textOnFormElementBackground};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  border-radius: 2px;
+  background: ${props => props.theme.formElementBackground};
+`
