58be284ba6633153e14a323ea0df7b6bdfcb238e
wpannell <wil.pannell@gmail.com>
2022-08-29 11:21:53 -0700

    let user switch the theme

--- src/index.js
+++ src/index.js
@@ -9,7 +9,27 @@ import {
   TertiaryButton,
 } from './components/Buttons'
 
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false)
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{margin: '0 16px 24px', padding: '8px', background: 'none'}}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark theme
+      </button>
 
+      <button
+        style={{margin: '0 16px 24px', padding: '8px', background: 'none'}}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default theme
+      </button>
+
+    </ThemeProvider>
+  )
+}
 
 const container = document.querySelector('#root')
 const root = createRoot(container)
