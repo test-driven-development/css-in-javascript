f3ba8f5ecc5bef6a1589d8c21da6c81fc3eeb28e
wpannell <wil.pannell@gmail.com>
2022-08-29 11:15:41 -0700

    prepare move div

--- src/index.js
+++ src/index.js
@@ -9,37 +9,7 @@ import {
   TertiaryButton,
 } from './components/Buttons'
 
-const App = () => (
-  <div>
-    <p>normal</p>
-    <PrimaryButton>Primary</PrimaryButton>
-    <SecondaryButton>Secondary</SecondaryButton>
-    <TertiaryButton>Tertiary</TertiaryButton>
 
-    <p>small medium large</p>
-    <PrimaryButton modifiers={['small']}>Primary</PrimaryButton>
-    <SecondaryButton>Secondary</SecondaryButton>
-    <TertiaryButton modifiers="large">Tertiary</TertiaryButton>
-
-    <p>disabled</p>
-    <PrimaryButton disabled>Primary</PrimaryButton>
-    <SecondaryButton disabled>Secondary</SecondaryButton>
-    <TertiaryButton disabled>Tertiary</TertiaryButton>
-
-    <p>success, warning, error</p>
-    <PrimaryButton modifiers={['success', 'primaryButtonSuccess']}>
-      Primary
-    </PrimaryButton>
-    <SecondaryButton modifiers={['warning', 'secondaryButtonWarning']}>
-      Secondary
-    </SecondaryButton>
-    <TertiaryButton modifiers={['error', 'tertiaryButtonError']}>
-      Tertiary
-    </TertiaryButton>
-
-    <GlobalStyle />
-  </div>
-)
 
 const container = document.querySelector('#root')
 const root = createRoot(container)
