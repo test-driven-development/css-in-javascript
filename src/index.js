1dc7cbc65e31b27663a88bc51cdf62a76e40eadc
wpannell <wil.pannell@gmail.com>
2022-08-29 08:19:18 -0700

    improve legibility

--- src/index.js
+++ src/index.js
@@ -13,14 +13,17 @@ const App = () => (
     <PrimaryButton>Primary</PrimaryButton>
     <SecondaryButton>Secondary</SecondaryButton>
     <TertiaryButton>Tertiary</TertiaryButton>
+
     <p>small medium large</p>
     <PrimaryButton modifiers={['small']}>Primary</PrimaryButton>
     <SecondaryButton>Secondary</SecondaryButton>
     <TertiaryButton modifiers="large">Tertiary</TertiaryButton>
+
     <p>disabled</p>
     <PrimaryButton disabled>Primary</PrimaryButton>
     <SecondaryButton disabled>Secondary</SecondaryButton>
     <TertiaryButton disabled>Tertiary</TertiaryButton>
+
     <GlobalStyle />
   </div>
 )
