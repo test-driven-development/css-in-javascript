1b5d32b2f736103f0ff87a0088a0fd0d9d74697b
wpannell <wil.pannell@gmail.com>
2022-08-29 11:22:17 -0700

    move the original UI

--- src/index.js
+++ src/index.js
@@ -27,6 +27,43 @@ const App = () => {
         Default theme
       </button>
 
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: '100vw',
+          height: '90vh',
+        }}
+      >
+        <p>normal</p>
+        <PrimaryButton>Primary</PrimaryButton>
+        <SecondaryButton>Secondary</SecondaryButton>
+        <TertiaryButton>Tertiary</TertiaryButton>
+
+        <p>small medium large</p>
+        <PrimaryButton modifiers={['small']}>Primary</PrimaryButton>
+        <SecondaryButton>Secondary</SecondaryButton>
+        <TertiaryButton modifiers="large">Tertiary</TertiaryButton>
+
+        <p>disabled</p>
+        <PrimaryButton disabled>Primary</PrimaryButton>
+        <SecondaryButton disabled>Secondary</SecondaryButton>
+        <TertiaryButton disabled>Tertiary</TertiaryButton>
+
+        <p>success, warning, error</p>
+        <PrimaryButton modifiers={['success', 'primaryButtonSuccess']}>
+          Primary
+        </PrimaryButton>
+        <SecondaryButton modifiers={['warning', 'secondaryButtonWarning']}>
+          Secondary
+        </SecondaryButton>
+        <TertiaryButton modifiers={['error', 'tertiaryButtonError']}>
+          Tertiary
+        </TertiaryButton>
+
+        <GlobalStyle />
+      </div>
     </ThemeProvider>
   )
 }
