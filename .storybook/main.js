ce1f857e913e22b1c1d8eb1bd844daae73b3bd48
wil.pannell <wpannel4@ford.com>
2022-08-30 01:04:01 -0700

    role back storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,13 +2,14 @@ module.exports = {
   stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
     '@storybook/preset-create-react-app',
-    '@storybook/addon-docs',
     '@storybook/addon-contexts/register',
     '@storybook/addon-backgrounds/register',
     '@storybook/addon-actions',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
-  },
 }
