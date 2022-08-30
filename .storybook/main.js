e02abef4753aaa4e85ea423a0857d803837a8815
wil.pannell <wpannel4@ford.com>
2022-08-30 00:14:51 -0700

    configure storybook for themes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.(js|mdx)'],
-  addons: ['@storybook/preset-create-react-app', '@storybook/addon-docs'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-docs',
+    '@storybook/addon-contexts/register',
+    '@storybook/addon-backgrounds/register',
+  ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
